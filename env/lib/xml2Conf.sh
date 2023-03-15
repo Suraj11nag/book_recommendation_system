d073598620e39e016984b4eb010b48a268c649f2
Suraj Nag <suraj11nag@gmail.com>
2023-03-15 23:45:05 +0530

    first commit

---
+++ env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/surajnag/Documents/book_recommendation_system/env/lib"
+XML2_LIBS="-lxml2 -L/Users/surajnag/Documents/book_recommendation_system/env/lib -lz -L/Users/surajnag/Documents/book_recommendation_system/env/lib -llzma -lpthread -L/Users/surajnag/Documents/book_recommendation_system/env/lib -liconv -L/Users/surajnag/Documents/book_recommendation_system/env/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/Users/surajnag/Documents/book_recommendation_system/env/include/libxml2 -I/Users/surajnag/Documents/book_recommendation_system/env/include"
+MODULE_VERSION="xml2-2.9.14"
+
---
+++ env/lib/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Users/surajnag/Documents/book_recommendation_system/env/lib"
+XSLT_LIBS="-lxslt -L/Users/surajnag/Documents/book_recommendation_system/env/lib -lxml2 -L/Users/surajnag/Documents/book_recommendation_system/env/lib -L/Users/surajnag/Documents/book_recommendation_system/env/lib -lpthread -L/Users/surajnag/Documents/book_recommendation_system/env/lib -liconv -L/Users/surajnag/Documents/book_recommendation_system/env/lib -licui18n -licuuc -licudata -lm "
+XSLT_PRIVATE_LIBS=""
+XSLT_INCLUDEDIR="-I/Users/surajnag/Documents/book_recommendation_system/env/include"
+MODULE_VERSION="xslt-1.1.35"
