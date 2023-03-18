e467d2a4f62d817293fe63fde357624453b8f90f
Suraj Nag <suraj11nag@gmail.com>
2023-03-18 19:37:18 +0530

    updated the app.py with new route

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template
 import pickle
 
 popular_df = pickle.load(open('popular.pkl','rb'))
+pivot_table = pickle.load(open('pivot_table.pkl','rb'))
+books = pickle.load(open('books.pkl','rb'))
+similarity_score = pickle.load(open('similarity_score.pkl','rb'))
 
 app = Flask(__name__)
 
@@ -18,6 +21,11 @@ def index():
 @app.route('/recommend')
 def recommend_ui():
     return render_template('recommend.html')
+
+@app.route('/recommend_books', methods=['post'])
+def recommend():
+    user_input = request.form.get('user_input')
+    return str(user_input)
 
 if __name__=='__main__':
     app.run(debug=True)
